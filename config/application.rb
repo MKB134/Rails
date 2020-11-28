e7471ed4a6d8fde2d0a0251257ab18f550f13b65
RubyRockstar <murad1110488@gmail.com>
2020-11-28 16:21:50 +0300

    Время_Местоположение

--- config/application.rb
+++ config/application.rb
@@ -9,8 +9,14 @@ Bundler.require(*Rails.groups)
 module TestGuru
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
+  config.load_defaults 6.0
 
+
+  config.i18n.default_locale = :ru
+  config.i18n.available_locales = [:ru, :en]
+  config.i18n.enforce_available_locales = true
+  config.time_zone = 'Moscow'
+  config.active_record.default_timezone = :local
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
