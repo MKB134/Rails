c78f2e16aa8c85385cd5fd23c6b23a4b64e7c535
RubyRockstar <murad1110488@gmail.com>
2021-07-09 17:01:52 +0300

    create/table/question

--- db/migrate/20210709140138_create_questions.rb
+++ db/migrate/20210709140138_create_questions.rb
@@ -2,8 +2,7 @@ class CreateQuestions < ActiveRecord::Migration[6.0]
   def change
     create_table :questions do |t|
       t.string :body
-      t.string :test_id
-      t.integer :
+      t.integer :test_id
 
       t.timestamps
     end
