93653e7782f293bcb5d2a34866c98d31db395b95
RubyRockstar <murad1110488@gmail.com>
2021-07-12 14:11:22 +0300

    11212

--- Gemfile
+++ Gemfile
@@ -6,7 +6,7 @@ ruby '2.6.3'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 6.0.3', '>= 6.0.3.4'
 # Use sqlite3 as the database for Active Record
-gem 'sqlite3', '~> 1.4'
+#gem 'pg'
 # Use Puma as the app server
 gem 'puma', '~> 4.1'
 # Use SCSS for stylesheets
@@ -52,3 +52,4 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+gem 'pg', '~> 1.0'
--- db/schema.rb
+++ db/schema.rb
@@ -10,50 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_09_141746) do
+ActiveRecord::Schema.define(version: 0) do
 
-  create_table "answers", force: :cascade do |t|
-    t.string "body", null: false
-    t.boolean "correct", null: false
-    t.integer "question_id_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["question_id_id"], name: "index_answers_on_question_id_id"
-  end
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
-  create_table "categories", force: :cascade do |t|
-    t.string "title", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "questions", force: :cascade do |t|
-    t.string "body", null: false
-    t.integer "test_id_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["test_id_id"], name: "index_questions_on_test_id_id"
-  end
-
-  create_table "tests", force: :cascade do |t|
-    t.string "title", null: false
-    t.integer "level", default: 0, null: false
-    t.integer "category_id_id", null: false
-    t.integer "user_id_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["category_id_id"], name: "index_tests_on_category_id_id"
-    t.index ["user_id_id"], name: "index_tests_on_user_id_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "name", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  add_foreign_key "answers", "question_ids"
-  add_foreign_key "questions", "test_ids"
-  add_foreign_key "tests", "category_ids"
-  add_foreign_key "tests", "user_ids"
 end
