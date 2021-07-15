b8df11ce328e29434e05fb85353f7e3595ad1253
RubyRockstar <murad1110488@gmail.com>
2021-07-15 15:54:11 +0300

    fix_foreign_key

--- db/migrate/20210712134334_create_answers.rb
+++ db/migrate/20210712134334_create_answers.rb
@@ -3,7 +3,7 @@ class CreateAnswers < ActiveRecord::Migration[6.0]
     create_table :answers do |t|
       t.string :body, null: false
       t.boolean :correct, null: false
-      t.references :question, null: false, foreign_key: {to_table: :questions}
+      t.references :question, null: false, foreign_key: true
 
       t.timestamps
     end
