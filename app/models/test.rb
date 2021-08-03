ce5b5c971c14a45771d44e147b8299c81f4c4847
RubyRockstar <murad1110488@gmail.com>
2021-08-03 20:42:16 +0300

    create_association_author/test

--- app/models/test.rb
+++ app/models/test.rb
@@ -3,6 +3,7 @@ class Test < ApplicationRecord
   belongs_to :category
   has_many :results
   has_many :users, through: :results
+  belongs_to :author, class_name: 'User', optional: true
 
   def self.titles_by_category(category_name)
     Test
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
-  def corses_by_level(level)
-    has_many :results
-    has_many :tests, through: :results
+  has_many :results
+  has_many :tests, through: :results
+   has_many :author, class_name: 'Test', foreign_key: 'author_id'
 
+  def corses_by_level(level)
     Test
       .joins("JOIN results ON tests.id = results.test_id")
       .where(results: {user_id: self.id}, tests: {level: level})
