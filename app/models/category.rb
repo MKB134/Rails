4d595ebd15bd2ec36b7320654bad925e6c49e43f
RubyRockstar <murad1110488@gmail.com>
2021-08-05 16:47:20 +0300

    add_in_category_nullify

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,3 @@
 class Category < ApplicationRecord
-  has_many :tests
+  has_many :tests, dependent: :nullify
 end
--- db/migrate/20210715152752_create_tests.rb
+++ db/migrate/20210715152752_create_tests.rb
@@ -3,7 +3,7 @@ class CreateTests < ActiveRecord::Migration[6.0]
     create_table :tests do |t|
       t.string :title, null: false
       t.integer :level, null: false, default: 0
-      t.references :category, null: false, foreign_key: true
+      t.references :category, null: true, foreign_key: true
       t.references :author, null: true, foreign_key:  {to_table: :users}
 
       t.timestamps
