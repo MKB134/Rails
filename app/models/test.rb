1f2ac44386b3dfac1e05f236b86e831f1ad6e3fb
RubyRockstar <murad1110488@gmail.com>
2021-08-16 17:29:34 +0300

    Validates

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,6 +2,10 @@ class Test < ApplicationRecord
   belongs_to :category, optional: true
   belongs_to :author, class_name: 'User', optional: true
 
+  validates :title, presense: true
+  validates :level, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :title, uniqueness: { scope: :level }
+
   has_many :questions, dependent: :destroy
   has_many :results, dependent: :destroy
   has_many :users, through: :results
@@ -9,15 +13,12 @@ class Test < ApplicationRecord
   scope :easy, -> { where(level: 0..1) }
   scope :medium, -> { where(level: 2..4) }
   scope :hard, -> {where(level: 5..Float::INFINITY)}
-
-  validates :level, numericality: { only_integer: true }
-  validates :title, presense: true, uniqueness: { scope: :level }
-
-  def self.titles_by_category(category_name)
-    Test
-      .joins(:category)
+  scope :by_category, -> (category_name) {
+      joins(:category)
       .where(categories: { title: category_name })
-      .order(title: :desc)
-      .pluck(:title)
+      .order(title: :desc) }
+
+  def self.titles_by_category(title)
+    by_category(title).pluck(:title)
   end
 end
