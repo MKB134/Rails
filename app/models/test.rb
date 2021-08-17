fe02d04537e180534a96ac2009dad18977b06e03
RubyRockstar <murad1110488@gmail.com>
2021-08-17 09:10:15 +0300

    fix_test

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,7 +2,7 @@ class Test < ApplicationRecord
   belongs_to :category, optional: true
   belongs_to :author, class_name: 'User', optional: true
 
-  validates :title, presense: true
+  validates :title, presence: true
   validates :level, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :title, uniqueness: { scope: :level }
 
