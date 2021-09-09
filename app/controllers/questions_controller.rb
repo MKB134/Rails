3ac6e98a89713ecc2dd017487d46f837adfeeee2
RubyRockstar <murad1110488@gmail.com>
2021-09-09 12:49:16 +0300

    Add_collback_and_fix

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,22 +1,47 @@
 class QuestionsController < ApplicationController
+
+  before_action :find_test, only: %i[ index create new ]
+  before_action :find_question, only: %i[ show  destroy]
+
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
+
   def index
-    test = Test.find(params[:test_id])
-    render json: { questions: test.questions }
+    render json: { questions: @test.questions }
   end
 
   def show
-    question = Question.find(params[:id])
-    render json: question
+    render json: @question
   end
 
   def new
 
   end
 
-  def create
-    result = ["Class: #{params.class}","Parameters: #{params.inspect}"]
-
-    render plain: result.join("\n")
+  def destroy
+    @question.destroy
+    render json: @question
   end
 
+  def create
+    @question = @test.questions.create(question_params)
+      render plain: @question.inspect
+  end
+
+  private
+
+  def find_question
+    @question = Question.find(params[:id])
+  end
+
+  def find_test
+    @test = Test.find(params[:test_id])
+  end
+
+  def question_params
+    params.require(:question).permit(:body)
+  end
+
+  def rescue_with_question_not_found
+    render plain: 'Question was not found'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'tests#index'
+
+    root to: 'tests#index'
   resources :tests do
     resources :questions, shallow: true
   end
