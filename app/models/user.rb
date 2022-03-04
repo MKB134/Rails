9e9a680664b8907ac31af163c205e750a2490774
RubyRockstar <murad1110488@gmail.com>
2022-03-04 14:52:15 +0300

    add_has_sequre_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,30 +11,13 @@ class User < ApplicationRecord
   validates :password, presence: true, if: Proc.new { |u| u.password_digest.blank? }
   validates :password, confirmation: true
 
+  has_secure_password
+
   def show_passed_by_level(level)
     tests.where(level: level)
   end
 
   def test_passage(test)
     test_passages.order(id: :desc).find_by(test_id: test.id)
-  end
-
-  def authenticate(password_string)
-    digest(password_string) == self.password_digest ? self : false
-  end
-
-  def password=(password_string)
-    if password_string.nil?
-      self.password_digest = nil
-    elsif password_string.present?
-      @password = password_string
-      self.password_digest = digest(password_string)
-    end
-  end
-
-  private
-
-  def digest(string)
-    Digest::SHA1.hexdigest(string)
   end
 end
