18a6d073c69bb6d09f0ff2c2159eaccd8f148e2e
RubyRockstar <murad1110488@gmail.com>
2022-03-18 20:18:24 +0300

    fix

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -22,7 +22,7 @@ class Admin::TestsController <  Admin::BaseController
   end
 
   def create
-    @test = current_user.created_tests.new(test_params)
+    @test = current_user.author.new(test_params)
     if @test.save
       redirect_to admin_test_path(@test)
     else
@@ -36,6 +36,7 @@ class Admin::TestsController <  Admin::BaseController
   end
 
   private
+
 
   def find_test
     @test = Test.find(params[:id])
