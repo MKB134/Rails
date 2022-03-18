e324a102d2146c867b1f5fc8fc0ce92f219e3f11
RubyRockstar <murad1110488@gmail.com>
2022-03-18 12:15:33 +0300

    add_first_name_last_name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,19 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
+  private
+
   def after_sign_in_path_for(user)
-   if user.is_a?(Admin)
-     admin_tests_path
-   else
+    if user.is_a?(Admin)
+      admin_tests_path
+    else
      tests_path
-   end
- end
+    end
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users, path: :gurus, path_names: { sign_in: :login, sign_out: :logout }
+  devise_for :users, path: :gurus, path_names: { sign_in: :login, sign_out: :logout }, controllers: { sessions: 'users/sessions' }
 
   root to: 'tests#index'
 
