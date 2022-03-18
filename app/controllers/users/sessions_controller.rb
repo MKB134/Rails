6e129f797c9c4dd321df72e7ba9e4c92b1f2dc02
RubyRockstar <murad1110488@gmail.com>
2022-03-18 12:25:13 +0300

    add_flash_hello

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -9,9 +9,10 @@ class Users::SessionsController < Devise::SessionsController
   # end
 
   # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+   def create
+     super
+     flash[:notice] = "Привет, #{current_user.first_name}!"
+   end
 
   # DELETE /resource/sign_out
   # def destroy
