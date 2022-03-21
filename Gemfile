6f7299a3a19116feaf6db4e25bf37794792fd5cf
RubyRockstar <murad1110488@gmail.com>
2022-03-21 19:47:07 +0300

    add_devise_and_default_URL

--- Gemfile
+++ Gemfile
@@ -26,6 +26,9 @@ gem 'devise', '~>4.0'
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
 
+#Localization
+gem 'rails-i18n'
+
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
   def default_url_options
-    { lang: I18n.locale }
+    I18n.locale == I18n.default_locale ? {} : { lang: I18n.locale }
   end
 
   private
