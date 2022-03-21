38614da02403d9e2b15fe04773920a552282e4f2
RubyRockstar <murad1110488@gmail.com>
2022-03-21 15:30:42 +0300

    localize_admin_tests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
+
+  def default_url_options
+    { lang: I18n.locale }
+  end
 
   private
 
@@ -15,5 +20,9 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+  end
+
+  def set_locale
+    I18n.locale = I18n.locale_available?(params[:lang]) ? params[:lang] : I18n.default_locale
   end
 end
