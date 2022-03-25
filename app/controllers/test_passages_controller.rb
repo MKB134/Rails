e46c017b03e06233d3b2c3b465d34286962191e2
RubyRockstar <murad1110488@gmail.com>
2022-03-25 14:31:26 +0300

    add_gist_method

--- app/controllers/test_passages_controller.rb
+++ app/controllers/test_passages_controller.rb
@@ -1,6 +1,6 @@
 class TestPassagesController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_test_passage, only: %i[show update result]
+  before_action :set_test_passage, only: %i[show update result gist]
 
   def show; end
 
@@ -15,6 +15,17 @@ class TestPassagesController < ApplicationController
     else
       render :show
     end
+  end
+
+  def gist
+    result = GistQuestionService.new(@test_passage.current_question).call
+     
+    flash_options = if result.success?
+      { notice: t('.success') }
+    else
+      { alert: t('.failure') }
+    end
+    redirect_to @test_passage, flash_options
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   resources :test_passages, only: %i[show update] do
     member do
       get :result
+      post :gist
     end
   end
 
