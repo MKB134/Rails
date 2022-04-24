430f448a9801a15ee48ad95d71600d4db14b363a
RubyRockstar <murad1110488@gmail.com>
2022-04-24 18:48:15 +0300

    add_data_attributes

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -1,9 +1,8 @@
 class Admin::TestsController <  Admin::BaseController
-  before_action :find_test, only: %i[show edit destroy update start]
+  before_action :set_tests, only: %i[index update_inline]
+  before_action :find_test, only: %i[show edit destroy update start update_inline]
 
-  def index
-    @tests = Test.all
-  end
+  def index; end
 
   def show; end
 
@@ -21,6 +20,15 @@ class Admin::TestsController <  Admin::BaseController
     end
   end
 
+  def update_inline
+    if @test.update(test_params)
+      redirect_to admin_tests_path
+    else
+      render :index
+    end
+  end
+
+
   def create
     @test = current_user.author.new(test_params)
     if @test.save
@@ -37,6 +45,9 @@ class Admin::TestsController <  Admin::BaseController
 
   private
 
+  def set_tests
+    @tests = Test.all
+  end
 
   def find_test
     @test = Test.find(params[:id])
