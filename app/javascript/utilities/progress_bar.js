d6c803d05449e3a28b22e1707ca807d27e9bc658
RubyRockstar <murad1110488@gmail.com>
2022-04-26 22:27:13 +0300

    fix_progress_bar

--- app/javascript/utilities/progress_bar.js
+++ app/javascript/utilities/progress_bar.js
@@ -1,13 +1,13 @@
 document.addEventListener('turbolinks:load', function(){
-  const progress_bar = document.querySelector(".progress-bar")
+  const progress_bar = document.querySelector(".progress-bar");
 
   if (progress_bar) {
-    const question_number = progress_bar.dataset.question_number
-    const question_count = progress_bar.dataset.question_count
-
-    const progressPercent = (question_number / question_count) * 100
-
-    progress_bar.style.width = progressPercent + "%"
+    const questionNumber = progress_bar.dataset.questionNumber;
+    const questionCount = progress_bar.dataset.questionCount;
     console.log(progress_bar)
+
+    const progressPercent = (questionNumber / questionCount) * 100;
+    progress_bar.style.width = progressPercent + "%"
+
   }
-})
+});
