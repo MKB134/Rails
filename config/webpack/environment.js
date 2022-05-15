cd5d11f7291e362c7cc25b6d4148991ef14d26cc
RubyRockstar <murad1110488@gmail.com>
2022-05-15 21:12:13 +0300

    test_enviroment

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,11 +1,23 @@
 const { environment } = require('@rails/webpacker')
 
+const customConfig = {
+  resolve: {
+    fallback: {
+      dgram: false,
+      fs: false,
+      net: false,
+      tls: false,
+      child_process: false
+    }
+  }
+};
 
-const webpack = require('webpack')
-environment.plugins.prepend('Provide',
-  new webpack.ProvidePlugin({
-    $: 'jquery/src/jquery',
-    jQuery: 'jquery/src/jquery'
-  })
-)
+environment.config.delete('node.dgram')
+environment.config.delete('node.fs')
+environment.config.delete('node.net')
+environment.config.delete('node.tls')
+environment.config.delete('node.child_process')
+
+environment.config.merge(customConfig);
+
 module.exports = environment
