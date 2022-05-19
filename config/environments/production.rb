3b3124279d52d2f73f34dd992f2153509228babc
RubyRockstar <murad1110488@gmail.com>
2022-05-19 11:36:26 +0300

    fix_producrion

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "test_guru_production"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'obscure-sands-20776.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'pure-hollows-36034.herokuapp.com/' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
