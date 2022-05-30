c2bf4aeaf6955a85d1dc19b0e85ccec21329d448
MKB134 <murad1110488@gmail.com>
2022-05-30 13:17:18 +0300

    fix_qurrent_question

--- db/migrate/20210928152136_create_test_passages.rb
+++ db/migrate/20210928152136_create_test_passages.rb
@@ -3,7 +3,7 @@ class CreateTestPassages < ActiveRecord::Migration[6.0]
     create_table :test_passages do |t|
       t.references :user, null: false, foreign_key: true
       t.references :test, null: false, foreign_key: true
-      t.references :current_question, null: false, foreign_key: {to_table: :questions}
+      t.references :current_question, foreign_key: {to_table: :questions}
       t.integer :correct_questions, default: 0
 
       t.timestamps
