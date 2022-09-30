11ff5e7cc9fffa0bf0003e38fbc1668913fe18c3
MKB134 <murad1110488@gmail.com>
2022-09-30 23:39:56 +0300

    add_stylesheets

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,6 +14,7 @@ require("utilities/sorting.js")
 require("utilities/check_password.js")
 require("utilities/form_inline.js")
 require("utilities/progress_bar.js")
+require("stylesheets/application")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
