1c462c0e07bc4ebabfdf3c6d491f152e43bd4a69
MKB134 <murad1110488@gmail.com>
2022-10-02 17:58:27 +0300

    default URL host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
 
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'mysterious-wave-04606.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'enigmatic-brushlands-31897.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:               'smtp.gmail.com',
@@ -70,7 +70,7 @@ Rails.application.configure do
     user_name:            ENV['SMTP_USERNAME'],
     password:             ENV['SMTP_PASSWORD'],
     authentication:       'plain',
-    enable_starttls_auto: true } 
+    enable_starttls_auto: true }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
