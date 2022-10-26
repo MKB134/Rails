003230c99e90c5614be49a2942f9f8f725d76fa4
MKB134 <murad1110488@gmail.com>
2022-10-26 12:41:48 +0300

    add badges

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -4,8 +4,8 @@ class TestPassage < ApplicationRecord
   belongs_to :user
   belongs_to :test
   belongs_to :current_question, class_name: 'Question', optional: true
-  has_many :badges, through: :test_passage_badges
-  has_many :test_passage_badges, dependent: :delete_all
+  has_many :badges, through: :test_passages_badges
+  has_many :test_passages_badges, dependent: :delete_all
 
   before_validation :before_validation_set_first_question, on: :create
   before_validation :before_validation_set_next_question, on: :update
